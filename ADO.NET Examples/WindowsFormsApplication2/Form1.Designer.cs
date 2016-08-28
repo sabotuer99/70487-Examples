74fc2a907ce79220b6d77603b681a4c429610152
sabotuer99 <sabotuer99@gmail.com>
2016-08-28 00:44:25 -0600

    Added ADO.NET Examples project

---
+++ ADO.NET Examples/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEmployee = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmployee
+            // 
+            this.dataGridViewEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployee.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewEmployee.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewEmployee.Name = "dataGridViewEmployee";
+            this.dataGridViewEmployee.Size = new System.Drawing.Size(408, 310);
+            this.dataGridViewEmployee.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 310);
+            this.Controls.Add(this.dataGridViewEmployee);
+            this.Name = "Form1";
+            this.Text = "Employee Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEmployee;
+    }
+}
+
---
+++ ADO.NET Examples/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            var conn = new SqlConnection(connectionString);
+
+            //DisconnectedModelLoad(conn);
+            ConnectedModelLoad(conn);
+        }
+ 
+        private void DisconnectedModelLoad(SqlConnection conn){
+
+            //Disconnected Model
+            var da = new SqlDataAdapter("SELECT * FROM HumanResources.Employee", conn);
+            var cmb = new SqlCommandBuilder(da);
+            DataSet employees = new DataSet();
+            da.Fill(employees, "HumanResources.Employee");
+            dataGridViewEmployee.DataSource = employees.Tables[0];
+        }
+
+        private void ConnectedModelLoad(SqlConnection conn){
+            //Connected Model
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM HumanResources.Employee";
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            var dr = cmd.ExecuteReader();
+            var employees = new DataTable();
+            employees.Load(dr);
+            dataGridViewEmployee.DataSource = employees;
+            conn.Close();
+        }
+    }
+}
