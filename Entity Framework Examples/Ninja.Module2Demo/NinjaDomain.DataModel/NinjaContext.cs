6aa7c493d95e01d1470757ae8b571581d283dfba
sabotuer99 <sabotuer99@gmail.com>
2016-09-02 15:15:36 -0600

    Got Wpf Example working

--- Entity Framework Examples/Ninja.Module2Demo/NinjaDomain.DataModel/NinjaContext.cs
+++ Entity Framework Examples/Ninja.Module2Demo/NinjaDomain.DataModel/NinjaContext.cs
@@ -1,5 +1,9 @@
 ﻿using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
 using NinjaDomain.Classes;
+using NinjaDomain.Classes.Interfaces;
+using System;
 
 namespace NinjaDomain.DataModel
 {
@@ -8,5 +12,42 @@ namespace NinjaDomain.DataModel
         public DbSet<Ninja> Ninjas { get; set;}
         public DbSet<Clan> Clans { get; set; }
         public DbSet<NinjaEquipment> Equipment { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types().
+                Configure(c => c.Ignore("IsDirty"));
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var history in this.ChangeTracker.Entries()
+                        .Where(e => e.Entity is IModificationHistory && 
+                              (e.State == EntityState.Added || e.State == EntityState.Modified))
+                        .Select(e => e.Entity as IModificationHistory))
+            {
+                history.DateModified = DateTime.Now;
+                if(history.DateCreated == DateTime.MinValue){
+                    history.DateCreated = DateTime.Now;
+                }
+            }
+
+            int result = base.SaveChanges();
+
+            foreach (var history in this.ChangeTracker.Entries()
+                        .Where(e => e.Entity is IModificationHistory)
+                        .Select(e => e.Entity as IModificationHistory))
+            {
+
+                history.DateModified = DateTime.Now;
+                if (history.DateCreated == DateTime.MinValue)
+                {
+                    history.IsDirty = false;
+                }
+            }
+
+            return result;
+        }
     }
 }
