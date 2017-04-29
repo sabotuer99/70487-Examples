ac7e2ff46e8e61f78d9ec7452a59ad2b0955cc00
sabotuer99 <sabotuer99@gmail.com>
2017-04-29 01:00:59 -0600

    added programmatic configuration example

---
+++ Entity Framework Examples/Ninja.Module2Demo/ProgramatticConfig/PrgNinjaDbConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ProgramatticConfig
+{
+    public class PrgNinjaDbConfiguration : DbConfiguration
+    {
+        public PrgNinjaDbConfiguration()
+        {
+            this.SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+
+            SqlConnectionStringBuilder connBuilder = new SqlConnectionStringBuilder();
+            connBuilder.InitialCatalog = "NinjaDomain.DataModel.NinjaContext";
+            connBuilder.DataSource = ".\\SQLEXPRESS";
+            connBuilder.IntegratedSecurity = true;
+
+            DumbConnectionFactory scf = new DumbConnectionFactory(connBuilder.ToString());
+            //SqlConnectionFactory scf = new SqlConnectionFactory(connBuilder.ToString());
+
+            this.SetDefaultConnectionFactory(scf);
+            this.SetProviderServices("System.Data.SqlClient", SqlProviderServices.Instance);
+        } 
+    }
+
+    public class DumbConnectionFactory : IDbConnectionFactory
+    {
+        public DumbConnectionFactory(string connString)
+        {
+            this.connString = connString;
+        }
+
+        public string connString { get; set; }
+
+        public DbConnection CreateConnection(string nameOrConnectionString)
+        {
+            return new SqlConnection(connString);
+        }
+    }
+}
---
+++ Entity Framework Examples/Ninja.Module2Demo/ProgramatticConfig/Program.cs
@@ -0,0 +1,32 @@
+﻿using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramatticConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new PrgNinjaContext())
+            {
+                var ninjas = ctx.Ninjas.ToList();
+
+                foreach (var ninja in ninjas)
+                {
+                    Console.WriteLine(ninja.Id + ": " + ninja.Name);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+
+    [DbConfigurationType(typeof(PrgNinjaDbConfiguration))]
+    public class PrgNinjaContext : NinjaContext {}
+}
