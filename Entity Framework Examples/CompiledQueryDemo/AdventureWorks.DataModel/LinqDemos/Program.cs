b60b982e3c21b167fcd0687a46758593428bba01
sabotuer99 <sabotuer99@gmail.com>
2017-05-06 22:27:17 -0600

    more LINQ demos

--- Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/LinqDemos/Program.cs
+++ Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/LinqDemos/Program.cs
@@ -12,9 +12,65 @@ namespace LinqDemos
         {
             //Numeric();
             //Sorting();
-            SetOps();
+            //SetOps();
+            //InitSeq();
+            //Booleans();
 
             Console.Read();
+        }
+
+        static void Booleans()
+        {
+            var first = "The quick brown fox".Split(' ');
+            var second = "jumped over the".Split(' ');
+            var third = "lazy dog".Split(' ');
+
+            Console.WriteLine("Part 1: " + PrettyPrint(first));
+            Console.WriteLine("Part 2: " + PrettyPrint(second));
+            Console.WriteLine("Part 3: " + PrettyPrint(third));
+            Console.WriteLine("\n");
+            Console.WriteLine("                       |  Part 1  |  Part 2  |  Part 3  |");
+            Console.WriteLine("All words have 'e'?    |" + 
+                    Cell(first.All(w => w.Contains('e'))) + "|" +
+                    Cell(second.All(w => w.Contains('e')))+ "|" +
+                    Cell(third.All(w => w.Contains('e'))) + "|");
+            Console.WriteLine("Any words have 'z'?    |" + 
+                    Cell(first.Any(w => w.Contains('z'))) + "|" +
+                    Cell(second.Any(w => w.Contains('z'))) + "|" +
+                    Cell(third.Any(w => w.Contains('z'))) + "|");
+            Console.WriteLine("Contains 'the'?        |" + 
+                    Cell(first.Contains("the")) + "|" +
+                    Cell(second.Contains("the")) + "|" +
+                    Cell(third.Contains("the")) + "|");
+            Console.WriteLine("Seq Equals 'lazy dog?' |" + 
+                    Cell(first.SequenceEqual(new List<string>(){"lazy","dog"})) + "|" +
+                    Cell(second.SequenceEqual(new List<string>(){"lazy","dog"})) + "|" +
+                    Cell(third.SequenceEqual(new List<string>(){"lazy","dog"})) + "|");
+            Console.WriteLine("Naive == 'lazy dog?'   |" +
+                    Cell(first.Equals(new List<string>() { "lazy", "dog" })) + "|" +
+                    Cell(second.Equals(new List<string>() { "lazy", "dog" })) + "|" +
+                    Cell(third.Equals(new List<string>() { "lazy", "dog" })) + "|");
+
+        }
+
+
+
+
+
+        private static string Cell(bool p)
+        {
+            return (p.ToString().PadRight(7)).PadLeft(10);
+        }
+
+
+
+        static void InitSeq()
+        {
+            var repeat = Enumerable.Repeat("A", 5);
+            var empty = Enumerable.Empty<String>();
+
+            Console.WriteLine("Repeat: " + PrettyPrint(repeat));
+            Console.WriteLine("Empty: " + PrettyPrint(empty));
         }
 
         static void SetOps()
