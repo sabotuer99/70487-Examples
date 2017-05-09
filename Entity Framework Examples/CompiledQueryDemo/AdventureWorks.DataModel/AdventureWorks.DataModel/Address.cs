2bc9b28ac97290be064a5c60958d1d824f045ad2
sabotuer99 <sabotuer99@gmail.com>
2017-05-08 22:41:09 -0600

    fiddling with LINQ to Entities

--- Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/AdventureWorks.DataModel/Address.cs
+++ Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/AdventureWorks.DataModel/Address.cs
@@ -27,7 +27,7 @@ namespace AdventureWorks.DataModel
         public string City { get; set; }
         public int StateProvinceID { get; set; }
         public string PostalCode { get; set; }
-        public System.Data.Spatial.DbGeography SpatialLocation { get; set; }
+        public System.Data.Entity.Spatial.DbGeography SpatialLocation { get; set; }
         public System.Guid rowguid { get; set; }
         public System.DateTime ModifiedDate { get; set; }
     
---
+++ Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/LINQtoEntitiesDemos/CommandTextTest.cs
@@ -0,0 +1,31 @@
+﻿using AdventureWorks.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoEntitiesDemos
+{
+    partial class Program
+    {
+        public static void CommandTextTest()
+        {
+            var dbContext = new AdventureWorks2012Entities();
+            ObjectContext context = ((IObjectContextAdapter)dbContext).ObjectContext;
+
+            var custs = context.CreateObjectSet<Customer>().Where( c => c.Person.FirstName.Length < 4)
+                            as ObjectQuery<Customer>;
+
+            Console.WriteLine(custs.Count());
+
+            Console.WriteLine(custs.CommandText);
+
+            Console.WriteLine(custs.ToTraceString() + "\n\n");
+
+            Console.WriteLine(dbContext.Customers.Where(c => c.Person.FirstName.Equals("Bob")));
+        }
+    }
+}
