aa1cc71e447385455fd8336d879859498656f28a
sabotuer99 <sabotuer99@gmail.com>
2017-05-22 23:21:23 -0600

    Added WCF Data Service demo for AdventureWorks DB

---
+++ Entity Framework Examples/CompiledQueryDemo/AdventureWorks.DataModel/LinqDemos/Transform.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemos
+{
+    partial class Program
+    {
+        static void Transform()
+        {
+            var nums = Enumerable.Range(1, 10);
+            var abcs = "ABCDEFGHIJ".ToCharArray();
+
+            Console.WriteLine("Numbers: " + PrettyPrint(nums));
+            Console.WriteLine("Letters: " + PrettyPrint(abcs));
+
+            var joined = abcs.Take(4)
+                .Aggregate("", (last, e) => last += last + e.ToString());
+            Console.WriteLine("\nAggregate: " + joined);
+
+            var zipped = nums.Zip(abcs, (n, l) => n.ToString() + l.ToString());
+            Console.WriteLine("\nZipped: " + PrettyPrint(zipped));
+
+            var concat = nums.Take(5).Concat(nums.Take(5));
+            Console.WriteLine("\nConcat: " + PrettyPrint(concat));
+
+            var listlist = new List<List<string>>(){
+                new List<string>(){"The", "quick", "brown" },
+                new List<string>(){"fox", "jumped", "over" },
+                new List<string>(){"the", "lazy", "dog" }
+            };
+
+            Console.WriteLine("\n\n*** LIST OF LISTS ***");
+            Console.WriteLine("\nPre-Flattened: \n" + PrettyPrint(listlist, true));
+            Console.WriteLine("\nFlattened: \n" + PrettyPrint(listlist.SelectMany(s => s)));
+        }
+
+
+    }
+}
---
+++ WCF Data Services Examples/WCFDataServiceDemo/WCFDataService/WcfDataService.svc.cs
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace WCFDataService
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class WcfDataService : DataService<AdventureWorks2012Entities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+            config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
