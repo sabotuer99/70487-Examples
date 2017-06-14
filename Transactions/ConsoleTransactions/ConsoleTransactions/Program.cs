bf9d927ea0e05df0503f54cbee4cda5121e5d1e8
sabotuer99 <sabotuer99@gmail.com>
2017-06-13 22:44:03 -0600

    added basic Transactions example code

---
+++ Transactions/ConsoleTransactions/ConsoleTransactions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ConsoleTransactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var tx = new CommittableTransaction();
+                Transaction.Current = tx;
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(
+                            @"SELECT COUNT(*) FROM Person.Person WHERE PersonType = 'EM'", conn);
+
+                    var count = (int)cmd.ExecuteScalar();
+
+                    tx.Commit();
+                }
+                catch
+                {
+                    tx.Rollback();
+                }
+            }
+        }
+    }
+}
