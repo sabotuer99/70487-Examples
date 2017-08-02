f3c20f09afa65e5bc420bad62044526940f44548
sabotuer99 <sabotuer99@gmail.com>
2017-08-01 21:45:44 -0600

    created basic service

---
+++ WCF Examples/PetService/PetService/CatService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetService
+{
+    [ServiceContract]
+    public interface ICatService
+    {
+        [OperationContract]
+        string speak();
+    }
+
+
+    public class CatService : ICatService
+    {
+        public string speak()
+        {
+            return "Meow!";
+        }
+    }
+}
---
+++ WCF Examples/PetService/PetService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Description;
+
+namespace PetService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ServiceHost(typeof(CatService));
+
+            //service.AddServiceEndpoint(typeof(ICatService), new WSHttpBinding());
+            service.AddServiceEndpoint(typeof(ICatService), new WSHttpBinding(), "http://localhost:8081/pets/wsCat");
+
+            // Enable metadata exchange.  
+            //ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            //smb.HttpGetEnabled = true;
+            //service.Description.Behaviors.Add(smb);
+
+            service.Open();
+
+            Console.WriteLine("The service is ready.");
+            Console.WriteLine("Press <ENTER> to terminate service.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            service.Close();
+
+
+        }
+    }
+}
