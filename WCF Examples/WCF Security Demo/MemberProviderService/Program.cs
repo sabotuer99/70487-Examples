fb92369d8556306acfe231b4efc07874f37af5a7
Troy Whorten <twhorten@mwfbi.com>
2017-09-20 16:09:46 -0600

    futzing around with membership provider

---
+++ WCF Examples/WCF Security Demo/MemberProviderService/Program.cs
@@ -0,0 +1,35 @@
+﻿using ServiceShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberProviderService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Service)))
+            {
+                host.Open();
+                PrintServiceDescription(host);
+                Console.ReadKey();
+            }
+        }
+
+        private static void PrintServiceDescription(ServiceHost host)
+        {
+            Console.WriteLine("{0} is up and running with the following endpoints:",
+                host.Description.ServiceType.Name);
+            foreach (ServiceEndpoint endpoint in host.Description.Endpoints)
+            {
+                Console.WriteLine("Address: {0}  ({1})",
+                    endpoint.Address.ToString(), endpoint.Binding.Name);
+            }
+        }
+    }
+}
---
+++ WCF Examples/WCF Security Demo/MembershipProviderClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipProviderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new EchoService.ServiceClient())
+            {
+                client.ClientCredentials.UserName.UserName = "wcfuser";
+                client.ClientCredentials.UserName.Password = "blah";
+
+                Console.WriteLine(client.Echo("A message from the client"));
+                Console.ReadLine();
+            }
+        }
+    }
+}
