a32bc07a3c3f233b46b8e4007041c4997ae7944a
sabotuer99 <sabotuer99@gmail.com>
2018-01-24 13:52:18 -0700

    added template for authentication demos

---
+++ WebApi-Examples/DogApi/OwinBasicAuth/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OwinBasicAuth
+{
+    public class DemoController : ApiController
+    {
+        [Authorize]
+        [Route("api/Demo")]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Bingo!");
+        }
+    }
+}
---
+++ WebApi-Examples/DogApi/OwinBasicAuth/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Batch;
+using System.Web.Http.Routing;
+
+namespace OwinBasicAuth
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
